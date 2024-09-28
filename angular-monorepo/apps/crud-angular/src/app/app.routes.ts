a0699a8b8ad9d42a77e13464148e47920d160575
Guilherme Martins <guilhermemm.dev@gmail.com>
2024-09-28 21:42:39 +0100

    feat: add routes

--- angular-monorepo/apps/crud-angular/src/app/app.routes.ts
+++ angular-monorepo/apps/crud-angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Route } from '@angular/router';
+import { Routes } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const APP_ROUTES: Routes = [
+  // { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  // {
+  //   path: 'courses',
+  //   loadChildren: () => import('./courses/courses.routes').then(m => m.COURSES_ROUTES)
+  // }
+];
