091a0dba23ec7aaba372be3ea296952d09b571ad
Guilherme Martins <guilhermemm.dev@gmail.com>
2024-10-10 13:19:23 +0100

    feat: add table

--- angular-monorepo/apps/crud-angular/src/app/courses/courses/courses.component.ts
+++ angular-monorepo/apps/crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { Course } from './model/course';
+
+const MOCK_DATA: Course[] = [
+  { _id: '1', name: 'test1', category: 'Hydrogen' },
+  { _id: '2', name: 'test2', category: 'Helium' },
+];
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss',
 })
-export class CoursesComponent {}
+export class CoursesComponent {
+  courses: Course[] = MOCK_DATA;
+  displayedColumns = ['name', 'category'];
+}
